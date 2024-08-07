86719b5820f57449907c8db3bf9aa3eca14dbc95
Vlad Sunny <vladislavsunny@gmail.com>
2024-08-07 22:54:58 +0300

    Combat Camera

--- Assets/Scripts/ThirdPersonCam.cs
+++ Assets/Scripts/ThirdPersonCam.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace JJBG.Camera
 {
@@ -8,11 +9,18 @@ namespace JJBG.Camera
         [SerializeField] private Transform _orientation;
         [SerializeField] private Transform _player;
         [SerializeField] private Transform _playerObj;
+        [SerializeField] private Transform _combatLookAt;
         [SerializeField] private Rigidbody _rigidbody;
 
         [Header("Settings")]
-        [SerializeField] private float _rotationSpeed;
-        [SerializeField] private float _updateOrientationSpeed;
+        [SerializeField] private float _rotationSpeed = 7f;
+        [SerializeField] private float _updateOrientationSpeed = 10f;
+        [SerializeField] private CameraStyle _currentStyle = CameraStyle.Basic;
+
+        private enum CameraStyle {
+            Basic,
+            Combat
+        }
 
         private void Awake() {
             Cursor.lockState = CursorLockMode.Locked;
@@ -20,15 +28,31 @@ namespace JJBG.Camera
         }
 
         private void Update() {
-            Vector3 viewDir = _player.position - new Vector3(transform.position.x, _player.position.y, transform.position.z);
-            _orientation.forward = Vector3.Slerp(_orientation.forward, viewDir.normalized, Time.deltaTime * _updateOrientationSpeed);
+            if (_currentStyle == CameraStyle.Basic) {
+                Vector3 viewDir = _player.position - new Vector3(transform.position.x, _player.position.y, transform.position.z);
+                _orientation.forward = Vector3.Slerp(_orientation.forward, viewDir.normalized, Time.deltaTime * _updateOrientationSpeed);
 
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-            Vector3 inputDir = _orientation.forward * verticalInput + _orientation.right * horizontalInput;
+                float horizontalInput = Input.GetAxis("Horizontal");
+                float verticalInput = Input.GetAxis("Vertical");
+                Vector3 inputDir = _orientation.forward * verticalInput + _orientation.right * horizontalInput;
 
-            if (inputDir != Vector3.zero)
-                _playerObj.forward = Vector3.Slerp(_playerObj.forward, inputDir.normalized, Time.deltaTime * _rotationSpeed);
+                if (inputDir != Vector3.zero)
+                    _playerObj.forward = Vector3.Slerp(
+                        _playerObj.forward,
+                        inputDir.normalized,
+                        Time.deltaTime * _rotationSpeed
+                    );
+            }
+            else if (_currentStyle == CameraStyle.Combat) {
+                Vector3 dirToCombatLookAt = _combatLookAt.position - new Vector3(transform.position.x, _combatLookAt.position.y, transform.position.z);
+                _orientation.forward = Vector3.Slerp(_orientation.forward, dirToCombatLookAt.normalized, Time.deltaTime * _updateOrientationSpeed);
+            
+                _playerObj.forward = Vector3.Slerp(
+                    _playerObj.forward,
+                    dirToCombatLookAt.normalized,
+                    Time.deltaTime * _updateOrientationSpeed
+                );
+            }
         }
     }
 }
