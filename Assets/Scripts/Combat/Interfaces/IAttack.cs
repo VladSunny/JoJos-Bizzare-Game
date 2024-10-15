fef8ef889a42ea5a91e535dd1f8c3617e4d386c4
Vlad Sunny <vladislavsunny@gmail.com>
2024-10-15 12:06:57 +0300

    Clean up old combat

--- Assets/Scripts/Combat/Interfaces/IAttack.cs
+++
@@ -1,9 +0,0 @@
-using Cysharp.Threading.Tasks;
-
-namespace JJBG.Combat
-{
-    public interface IAttack
-    {
-        UniTask Attack();
-    }
-}
--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -1,44 +1,43 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-using JJBG.Combat;
 using DG.Tweening;
 
 namespace JJBG.UI
 {
     public class HealthBar : MonoBehaviour
     {
-        [Header("References")]
-        [SerializeField] private Health _health;
-        [SerializeField] private Image _fill;
+        // [Header("References")]
+        // [SerializeField] private Health _health;
+        // [SerializeField] private Image _fill;
 
-        [Header("Settings")]
-        [SerializeField] private Gradient _gradient;
-        [SerializeField] private float _sliderSpeed = 0.5f;
+        // [Header("Settings")]
+        // [SerializeField] private Gradient _gradient;
+        // [SerializeField] private float _sliderSpeed = 0.5f;
 
-        private Slider _slider;
-        private float _maxHealth;
+        // private Slider _slider;
+        // private float _maxHealth;
 
-        private void Awake() {
-            _slider = GetComponent<Slider>();
+        // private void Awake() {
+        //     _slider = GetComponent<Slider>();
 
-            _maxHealth = _health.GetMaxHealth();
+        //     _maxHealth = _health.GetMaxHealth();
 
-            _slider.minValue = 0;
-            _slider.maxValue = _maxHealth;
-        }
+        //     _slider.minValue = 0;
+        //     _slider.maxValue = _maxHealth;
+        // }
 
-        private void OnEnable() {
-            _health.onHealthChanged += OnHealthChanged;
-        }
+        // private void OnEnable() {
+        //     _health.onHealthChanged += OnHealthChanged;
+        // }
 
-        private void OnDisable() {
-            _health.onHealthChanged -= OnHealthChanged;
-        }
+        // private void OnDisable() {
+        //     _health.onHealthChanged -= OnHealthChanged;
+        // }
 
-        private void OnHealthChanged(float health) {
-            _slider.DOValue(health, _sliderSpeed).SetEase(Ease.OutCubic);
-            _fill.color = _gradient.Evaluate(health / _maxHealth);
-        }
+        // private void OnHealthChanged(float health) {
+        //     _slider.DOValue(health, _sliderSpeed).SetEase(Ease.OutCubic);
+        //     _fill.color = _gradient.Evaluate(health / _maxHealth);
+        // }
     }
 }
