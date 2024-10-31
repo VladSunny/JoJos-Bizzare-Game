334bb4e38b0507298c49545a1ecef012fe25d64f
Vlad Sunny <vladislavsunny@gmail.com>
2024-10-31 18:04:23 +0300

    Barraging & Finisher

--- Assets/Scripts/Combat/Skills/BarrageSkill.cs
+++ Assets/Scripts/Combat/Skills/BarrageSkill.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 using Cysharp.Threading.Tasks;
 
 using JJBG.Movement;
+using JJBG.Attributes;
 
 namespace JJBG.Combat
 {
     public class BarrageSkill : MonoBehaviour, ISkill
     {
-        [Header("Name of object should be name of clip in animator")]
-        [Space(20)]
+        // [Header("Name of object should be name of clip in animator")]
+        // [Space(20)]
 
         [Header("Dependencies")]
         [SerializeField] private Animator _animator;
@@ -18,38 +19,74 @@ namespace JJBG.Combat
         [SerializeField] private Rigidbody _rb;
         [SerializeField] private SPMovement _spMovement;
 
-        // [Header("Settings")]
-        // [SerializeField] private float _damage = 10f;
-        // [SerializeField] private float _knockback = 5f;
-        // [SerializeField] private float _enemyStunDuration = 2f;
-        // [SerializeField] private float _lunge = 5f;
-        // [SerializeField] private bool _makeRagdoll = false;
-        // [Tooltip("In milliseconds"), SerializeField] private int _punchDelay = 500;
-        // [SerializeField] private float _attackTime = 1f;
+        [Header("Clip Name")]
+        [SerializeField] private string _barrageAnimationClipName = "Barraging";
 
-        private string _basePunchClipName;
+        [Header("Settings")]
+        [SerializeField] private float _damage = 10f;
+        [SerializeField] private float _knockback = 5f;
+        [SerializeField] private float _enemyStunDuration = 2f;
+        [SerializeField] private float _lunge = 5f;
+        [SerializeField] private float _secondsBetweenAttacks = 0.5f;
+        [SerializeField] private float _timeForContinueCombo = 0.5f;
 
-        private void Awake()
-        {
-            _basePunchClipName = gameObject.name;
+        [Header("Timers")]
+        [SerializeField, ReadOnly] private float _attackTimer = 0f;
+
+        private bool _barraging = false;
+
+        private void Update() {
+            if (_barraging) {
+                if (_attackTimer > 0) {
+                    _attackTimer -= Time.deltaTime;
+                } else {
+                    _attackTimer = _secondsBetweenAttacks;
+                    _dynamicHitBox.CreateHitBox(Vector3.forward * 1f, new Vector3(1f, 1f, 1f), Hit, true);
+                }
+            }
+            else
+                _attackTimer = 0f;
         }
 
         public UniTask Attack()
         {
             _spMovement.SetAttackTimer(100f);
 
-            _animator.SetBool("Barraging", true);
+            _animator.SetBool(_barrageAnimationClipName, true);
+
+            _barraging = true;
 
             return UniTask.CompletedTask;
         }
 
         public UniTask Stop()
         {
-            _spMovement.SetAttackTimer(0f);
+            _spMovement.SetAttackTimer(_timeForContinueCombo);
 
-            _animator.SetBool("Barraging", false);
+            _animator.SetBool(_barrageAnimationClipName, false);
+
+            _barraging = false;
 
             return UniTask.CompletedTask;
+        }
+
+        private void Hit(Collider collider)
+        {
+            if (collider.gameObject == _player)
+                return;
+
+            Health health = collider.GetComponentInChildren<Health>();
+            StunManager stunManager = collider.GetComponentInChildren<StunManager>();
+
+            if (health) health.TakeDamage(_damage);
+            if (stunManager) stunManager.SetStun(_enemyStunDuration);
+
+            Rigidbody rb = collider.GetComponentInChildren<Rigidbody>();
+
+            if (rb) rb.AddForce(_playerObj.forward * _knockback, ForceMode.Impulse);
+
+            if (_rb)
+                _rb.AddForce(_playerObj.forward * _lunge, ForceMode.Impulse);
         }
     }
 }
